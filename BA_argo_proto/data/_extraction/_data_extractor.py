86124bd009d37b0703f96df98ac9d9766156f765
Sebastian <s0543196@htw-berlin.de>
2018-01-26 12:28:39 +0100

    proto - databasewriter - Better use of  SQLAlchemys model

--- BA_argo_proto/data/_extraction/_data_extractor.py
+++ BA_argo_proto/data/_extraction/_data_extractor.py
@@ -4,12 +4,10 @@ from ._float import Float
 
 class ExtractorFactory:
     def __init__(self, data_directory):
-        self.data_directory = data_directory
-
-        self.extractor_generator = (  # Comprehension with () is a generator.
-            Float(os.path.join(self.data_directory, profile_directory)) for
-            profile_directory in os.listdir(self.data_directory)
-        )
+        self.extractor_generator = (
+            Float(os.path.join(data_directory,
+                               profile_directory))
+            for profile_directory in os.listdir(data_directory))
 
     def get_data_sets(self):
         return self.extractor_generator
