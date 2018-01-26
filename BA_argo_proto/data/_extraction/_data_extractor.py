d6c9a0b0740a4c175a51142a3af52d19ccf45341
Sebastian <s0543196@htw-berlin.de>
2018-01-26 12:25:56 +0100

    proto - data_extractor - Better Factory pattern.

--- BA_argo_proto/data/_extraction/_data_extractor.py
+++ BA_argo_proto/data/_extraction/_data_extractor.py
@@ -4,19 +4,12 @@ from ._float import Float
 
 class ExtractorFactory:
     def __init__(self, data_directory):
-        """
-        Factory for Argo Float data-extractors.
-        
-        :param data_directory: PATH to the Argo_Data directory.
-        """
         self.data_directory = data_directory
 
+        self.extractor_generator = (  # Comprehension with () is a generator.
+            Float(os.path.join(self.data_directory, profile_directory)) for
+            profile_directory in os.listdir(self.data_directory)
+        )
+
     def get_data_sets(self):
-        """
-        The whole dataset of the argo-floats.
-        
-        :return: Generator-object holding Argo-Floats
-        """
-        for profile_directory in os.listdir(self.data_directory):
-            yield Float(os.path.join(self.data_directory,
-                                     profile_directory))
+        return self.extractor_generator
