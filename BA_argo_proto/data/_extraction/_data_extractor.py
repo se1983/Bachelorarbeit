71440f31b5db6f9be332c9856087c245a6d24ee2
Sebastian <s0543196@htw-berlin.de>
2018-01-31 22:05:48 +0100

    proto - data_extractor - Add attribute for size of the generator

--- BA_argo_proto/data/_extraction/_data_extractor.py
+++ BA_argo_proto/data/_extraction/_data_extractor.py
@@ -2,12 +2,19 @@ import os
 
 from ._float import Float
 
+
 class ExtractorFactory:
     def __init__(self, data_directory):
+
         self.extractor_generator = (
             Float(os.path.join(data_directory,
                                profile_directory))
             for profile_directory in os.listdir(data_directory))
 
+        self.sum_floats = sum([os.path.isdir(d) for d in os.listdir(data_directory)])
+
     def get_data_sets(self):
         return self.extractor_generator
+
+    def float_count(self):
+        return self.sum_floats
