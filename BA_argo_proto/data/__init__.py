779445c80556a52ac43dc703250d105285a67b44
Sebastian <s0543196@htw-berlin.de>
2018-02-01 20:22:33 +0100

    proto - Extractor - Only generate the extractor if needed.

--- BA_argo_proto/data/__init__.py
+++ BA_argo_proto/data/__init__.py
@@ -3,9 +3,5 @@ from ._extraction import ExtractorFactory
 from ._insertion import DataBaseWriter
 
 
-_data_folder = app.config['data_folder']
-_data_sets = ExtractorFactory(_data_folder).get_data_sets()
 
-float_count = ExtractorFactory(_data_folder).float_count()
-db_writer = DataBaseWriter(argo_floats=_data_sets, db=db, app=app)
 
--- BA_argo_proto/manage.py
+++ BA_argo_proto/manage.py
@@ -1,11 +1,11 @@
 import urllib
-from copy import deepcopy
 
+from tqdm import tqdm
 from flask import url_for
 from flask_script import Manager, Server, Shell, prompt_bool
 
 from webapp import app, db
-from data import db_writer
+from data import ExtractorFactory, DataBaseWriter
 
 assert db is not None
 
@@ -20,17 +20,14 @@ def rebuild_db():
     print(f"Datafolder: {app.config['data_folder']}")
     if True or prompt_bool("Rebuild the database?"):
 
-        session = db.create_scoped_session(
-            options={
-                'bind': db.get_engine(app, 'data_input'),
-                'binds': {}
-            }
-        )
+        _extractor = ExtractorFactory(app.config['data_folder'])
+        float_count = _extractor.float_count()
+        db_writer = DataBaseWriter(argo_floats=_extractor.get_data_sets(), db=db, app=app)
 
         db.drop_all(bind=['data_input'], app=app)
         db.create_all(bind=['data_input'], app=app)
 
-        for argo_float in db_writer:
+        for argo_float in tqdm(db_writer, total=float_count):
             argo_float.write_data(bind='data_input')
 
 
@@ -38,22 +35,20 @@ def rebuild_db():
 def list_routes():
     """
     comes frome here: http://flask.pocoo.org/snippets/117/
-    :return:
     """
-    output = []
-    for rule in app.url_map.iter_rules():
+
+    def generate_route_string(rule):
         options = {arg: f"[{arg}]" for arg in rule.arguments}
 
         methods = ','.join(rule.methods)
         url = url_for(rule.endpoint, **options)
-        line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
-        output.append(line)
+        return urllib.parse.unquote(f"{rule.endpoint:50s} {methods:20s} {url}")
 
-    for line in sorted(output):
+    for line in [generate_route_string(rule) for rule in app.url_map.iter_rules()]:
         print(line)
 
 
-manager.add_command("runserver", Server())
+manager.add_command("runserver", Server(threaded=True))
 manager.add_command("shell", Shell())
 
 if __name__ == "__main__":
