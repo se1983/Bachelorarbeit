76a2ccfa224725f7d6b38eec9d5d65bf5a817ac8
Sebastian <s0543196@htw-berlin.de>
2018-02-07 10:25:54 +0100

    proto - map - Added sql script for requesting the positions of one argo.

---
+++ BA_argo_proto/webapp/templates/argo_positions.sql
@@ -0,0 +1,10 @@
+SELECT
+  locations.latitude,
+  locations.longitude,
+  profiles.timestamp
+FROM argo_floats
+  JOIN measurements ON argo_floats.id = measurements.argo_float_id
+  JOIN locations ON measurements.location_id = locations.id
+  JOIN profiles ON measurements.id = profiles.measurement_id
+WHERE identifier = '%s'
+ORDER BY profiles.timestamp;
