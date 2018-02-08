e150600b599336e12fa41c7a51914056e160feda
Sebastian <s0543196@htw-berlin.de>
2018-02-08 15:46:18 +0100

    proto - query_factory - Delete obsolete sqltemplates [#5]

--- BA_argo_proto/webapp/templates/argo_data.sql
+++
@@ -1,7 +0,0 @@
-SELECT *
-FROM argo_floats
-  JOIN measurements ON argo_floats.id = measurements.argo_float_id
-  JOIN locations ON measurements.location_id = locations.id
-  JOIN profiles ON measurements.id = profiles.measurement_id
-WHERE identifier = :var
-ORDER BY profiles.timestamp;
