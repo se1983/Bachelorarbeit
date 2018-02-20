480368d81f07d78f62c5056a6e93c59feba0d7a5
Sebastian <s0543196@htw-berlin.de>
2018-02-20 14:35:54 +0100

    proto - frontend - Plottling labels

--- BA_argo_proto/webapp/helpers/_generate_graph.py
+++ BA_argo_proto/webapp/helpers/_generate_graph.py
@@ -7,9 +7,15 @@ from matplotlib.figure import Figure
 from matplotlib.pyplot import style
 import matplotlib.patches as mpatches
 
+import numpy as np
+
 style.use('seaborn-whitegrid')
 matplotlib.rcParams['xtick.labelsize'] = 20
 matplotlib.rcParams['ytick.labelsize'] = 16
+matplotlib.rcParams['legend.shadow'] = True
+matplotlib.rcParams['legend.framealpha'] = 0.3
+matplotlib.rcParams['legend.facecolor'] = "b"
+
 
 
 def create_plot(data):
@@ -26,24 +32,27 @@ def create_plot(data):
     fig = Figure()
     fig.set_size_inches(8.5, 10.5)
 
-    ax1 = fig.add_subplot(grid[0])
-    ax1.set_xticklabels([])
-    patch = mpatches.Patch(color='dodgerblue', label='Temperatur\n$°C$')
-    ax1.legend(handles=[patch], prop={'size': 20}, loc=3)
-    ax1.plot(dates, temperature, '-', color='dodgerblue')
+    if not any(np.isnan(temperature)):
+        ax1 = fig.add_subplot(grid[0])
+        ax1.set_xticklabels([])
+        patch = mpatches.Patch(color='dodgerblue', label='Temperatur\n$°C$')
+        ax1.legend(handles=[patch], prop={'size': 20}, loc=3)
+        ax1.plot(dates, temperature, '-', color='dodgerblue')
 
-    ax2 = fig.add_subplot(grid[1])
-    ax2.set_xticklabels([])
-    patch = mpatches.Patch(color='dodgerblue', label='Salzgehalt\n$g/l$')
-    ax2.legend(handles=[patch], prop={'size': 20}, loc=3)
-    do_plot(ax2, (dates, salinity))
-    ax2.plot(dates, salinity, '-', color='dodgerblue')
+    if not any(np.isnan(salinity)):
+        ax2 = fig.add_subplot(grid[1])
+        ax2.set_xticklabels([])
+        patch = mpatches.Patch(color='dodgerblue', label='Salzgehalt\n$g/l$')
+        ax2.legend(handles=[patch], prop={'size': 20}, loc=3)
+        do_plot(ax2, (dates, salinity))
+        ax2.plot(dates, salinity, '-', color='dodgerblue')
 
-    ax3 = fig.add_subplot(grid[2])
-    patch = mpatches.Patch(color='dodgerblue', label='Druck')
-    ax3.legend(handles=[patch], prop={'size': 20}, loc=3)
-    do_plot(ax3, (dates, pressure))
-    ax3.plot(dates, pressure, '-', color='dodgerblue')
+    if not any(np.isnan(pressure)):
+        ax3 = fig.add_subplot(grid[2])
+        patch = mpatches.Patch(color='dodgerblue', label='Druck\n$p$')
+        ax3.legend(handles=[patch], prop={'size': 20}, loc=3)
+        do_plot(ax3, (dates, pressure))
+        ax3.plot(dates, pressure, '-', color='dodgerblue')
 
     fig.autofmt_xdate()
     fig.tight_layout()
