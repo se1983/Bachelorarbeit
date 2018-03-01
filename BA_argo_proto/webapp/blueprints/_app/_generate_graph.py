proto - generateGraph - If values overflow the defined maxval. The Plotting will be locket to ymin and ymax. This fixes #13

BA_argo_proto/webapp/blueprints/_app/_generate_graph.py
@@ -7,9 +7,13 @@ from matplotlib.figure import Figure
 from matplotlib.pyplot import style
 import matplotlib.patches as mpatches
 import matplotlib.dates as mdates
-
-
 import numpy as np
+
+from webapp import app
+
+ranges = app.config['ARGO_DATA_VALUE_RANGES']
+
+
 
 style.use('seaborn-whitegrid')
 matplotlib.rcParams['xtick.labelsize'] = 20
@@ -19,11 +23,10 @@ matplotlib.rcParams['legend.framealpha'] = 0.3
 matplotlib.rcParams['legend.facecolor'] = "b"
 
 
-
 def create_plot(data):
-    temperature = data['temperature']
-    salinity = data['salinity']
-    pressure = data['pressure']
+    temperature = np.array(data['temperature'])
+    salinity = np.array(data['salinity'])
+    pressure = np.array(data['pressure'])
     dates = sorted([datetime.fromtimestamp(
         mktime(strptime(d, '%a, %d %b %Y %H:%M:%S GMT'))) for i, d in enumerate(data['timestamp'])])
 
@@ -37,6 +40,11 @@ def create_plot(data):
     if not any(np.isnan(temperature)):
         ax1 = fig.add_subplot(grid[0])
         ax1.set_xticklabels([])
+
+        ymin, ymax = ranges['temperature']
+        if np.any(temperature > ymax):
+            ax1.set_ylim(ymax=ymax, ymin=ymin)
+
         patch = mpatches.Patch(color='dodgerblue', label='Temperatur [$°C$]')
         ax1.legend(handles=[patch], prop={'size': 20}, loc=3)
         ax1.plot(dates, temperature, '-', color='dodgerblue')
@@ -44,6 +52,11 @@ def create_plot(data):
     if not any(np.isnan(salinity)):
         ax2 = fig.add_subplot(grid[1])
         ax2.set_xticklabels([])
+
+        ymin, ymax = ranges['salinity']
+        if np.any(salinity > ymax):
+            ax2.set_ylim(ymax=ymax, ymin=ymin)
+
         patch = mpatches.Patch(color='dodgerblue', label='Salzgehalt [$g/l$]')
         ax2.legend(handles=[patch], prop={'size': 20}, loc=3)
         ax2.plot(dates, salinity, '-', color='dodgerblue')
@@ -51,6 +64,11 @@ def create_plot(data):
     if not any(np.isnan(pressure)):
         ax3 = fig.add_subplot(grid[2])
         patch = mpatches.Patch(color='dodgerblue', label='Druck [$p$]')
+
+        ymin, ymax = ranges['pressure']
+        if np.any(pressure > ymax):
+            ax3.set_ylim(ymax=ymax, ymin=ymin)
+
         ax3.legend(handles=[patch], prop={'size': 20}, loc=3)
         ax3.format_xdata = mdates.DateFormatter("%m")
         ax3.plot(dates, pressure, '-', color='dodgerblue')