95718ccc15150d9b059dc0e808cab82e32ca07dd
Sebastian <s0543196@htw-berlin.de>
2018-03-02 14:19:12 +0100

    proto - webapp - app - cenerate_graph: removed obsolete lines

--- BA_argo_proto/webapp/blueprints/_app/_generate_graph.py
+++ BA_argo_proto/webapp/blueprints/_app/_generate_graph.py
@@ -30,9 +30,6 @@ def create_plot(data):
     dates = sorted([datetime.fromtimestamp(
         mktime(strptime(d, '%a, %d %b %Y %H:%M:%S GMT'))) for i, d in enumerate(data['timestamp'])])
 
-    def do_plot(ax, data):
-        ax.plot(*data, '-')
-
     grid = gridspec.GridSpec(3, 1)
     fig = Figure()
     fig.set_size_inches(8.5, 10.5)
