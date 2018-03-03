8102a5ee473228f839ea983e421d3b2ee1703515
Sebastian <s0543196@htw-berlin.de>
2018-03-03 18:59:53 +0100

    doc - Lektoriat (2/2).

--- BA_argo_doc/scr/beispiele/sqlalchemy_read_query.py
+++ BA_argo_doc/scr/beispiele/sqlalchemy_read_query.py
@@ -1,6 +1,6 @@
 from webapp import db
 
-sql_template = "SELECT DISTINCT ON (f.id)
+sql_template = """SELECT DISTINCT ON (f.id)
   f.identifier,
   l.longitude,
   l.latitude,
@@ -12,6 +12,6 @@ FROM argo_floats f
     ON m.id = p.measurement_id
   JOIN locations l
     ON l.id = m.location_id
-ORDER BY f.id, p.cycle DESC;"
+ORDER BY f.id, p.cycle DESC;"""
 
 result = db.engine.execute(sql_template)
